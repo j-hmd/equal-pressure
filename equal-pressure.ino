bd6b1107e2f9b2f3243992a29431fdcb838e2350
juhamada13 <julianahanae@gmail.com>
2020-02-12 16:00:04 -0800

    initial commit

---
+++ equal-pressure.ino
@@ -0,0 +1,24 @@
+/*
+**** Author: Juliana Hamada ****
+**** Date: Feb 12 2020 *********
+*
+******************** Project: Equal Pressure for Realign ********************
+* 
+* Inputs: sensor output from flexiforce pressure sensors
+* Output: amount of force from both sensors 
+*/
+
+const int sensorOut = A0;           // Pin A0 to read anaog input
+
+int pressure;                       // Store analog value for pressure
+
+void setup() {
+  Serial.begin(9600);               // Start serial communication
+}
+
+void loop() {
+  // Main //
+  pressure = analogRead(sensorOut); // read sensor output
+  Serial.println(pressure);         // print pressure value
+  delay(100);                       // delay
+}
