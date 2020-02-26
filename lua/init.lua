520cdf40209693581e0b46ef83e2a2c72d0729a4
juhamada13 <julianahanae@gmail.com>
2020-02-26 07:47:57 -0800

    control built in led with web server

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,7 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("CXNK00605F35","7783026654")
 print(wifi.sta.getip())
-led1 = 2
+led1 = 0
 --led2 = 4
 gpio.mode(led1, gpio.OUTPUT)
 --gpio.mode(led2, gpio.OUTPUT)
@@ -24,32 +24,19 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        --buf = buf.."\n";
-        --buf = buf.."\n";
-        --buf = buf.."\n";
-        --buf = buf.."";
-        --buf = buf.."\n";
-        --buf = buf.." ESP8266 Web Server";
-        --buf = buf.."LED ON OFF";
-        --buf = buf.."GPIO2 ON OFF";
-        --buf = buf.."\n";
-        --buf = buf.."\n";
-        --buf = buf.."";
+        -- Buffering message to be sent
+        buf = buf.."<!DOCTYPE html>"
+        but = buf.."<body>"
         buf = buf.."<h1> ESP8266 Web Server</h1>";
         buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
-        --buf = buf.."<p>GPIO2 <a href=\"?pin=ON2\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF2\"><button>OFF</button></a></p>";
+        buf = buf.."</body>"
         local _on,_off = "",""
         if(_GET.pin == "ON1")then
-              gpio.write(led1, gpio.HIGH);
+              gpio.write(led1, gpio.LOW); -- because pin 0 ia active low
         elseif(_GET.pin == "OFF1")then
-              gpio.write(led1, gpio.LOW);
-        --elseif(_GET.pin == "ON2")then
-             -- gpio.write(led2, gpio.HIGH);
-       -- elseif(_GET.pin == "OFF2")then
-             -- gpio.write(led2, gpio.LOW);
+              gpio.write(led1, gpio.HIGH);
         end
         client:send("HTTP/1.1 200 OK\n\n");
-        --client:send("Content-type: text/html\r\n");
         client:send(buf);
         client:close()
         collectgarbage();
