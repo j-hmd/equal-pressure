2f95bf2dbbbaa23a32b71ab5d7664ac7ad1df05f
juhamada13 <julianahanae@gmail.com>
2020-03-20 20:50:01 -0700

    displayed pressure values in web page, closing issue #3

--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
 wifi.setmode(wifi.STATION)
-wifi.sta.config("CXNK00605F35","7783026654")
-print(wifi.sta.getip())
-led1 = 0
---led2 = 4
-gpio.mode(led1, gpio.OUTPUT)
---gpio.mode(led2, gpio.OUTPUT)
 
-if srv~=nil then
-  srv:close()
-end
+ssid = "CXNK00605F35"
+pwd= "7783026654"
+wifi.sta.config(ssid, pwd)
 
+wifi.sta.getip()
+
+-- Only one server is allowed
+if srv then srv:close() end
+
+-- Crating TCP connection
 srv=net.createServer(net.TCP)
-srv:listen(80,function(conn)
-    conn:on("receive", function(client,request)
+srv:listen(80,function(conn) -- net.server.listen([port],[ip],function(net.socket))
+    conn:on("receive", function(client,request) -- "connection", "reconnection", "disconnection", "receive" or "sent", and 
+        print(request)
         local buf = "";
         local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
         if(method == nil)then
@@ -28,17 +29,25 @@ srv:listen(80,function(conn)
         buf = buf.."<!DOCTYPE html>"
         but = buf.."<body>"
         buf = buf.."<h1> ESP8266 Web Server</h1>";
-        buf = buf.."<p>GPIO0 <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
+        buf = buf.."<p>Read Pressure <a href=\"?pin=ON1\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFF1\"><button>OFF</button></a></p>";
         buf = buf.."</body>"
         local _on,_off = "",""
+        local pressureTable = {}
         if(_GET.pin == "ON1")then
-              gpio.write(led1, gpio.LOW); -- because pin 0 ia active low
+            for i=1,100 do    
+                buf = buf..adc.read(0)
+                buf = buf.."<br>"
+            end
+              --end
+              --gpio.write(led1, gpio.LOW); -- because pin 0 ia active low
         elseif(_GET.pin == "OFF1")then
-              gpio.write(led1, gpio.HIGH);
+            -- reset
         end
+        
         client:send("HTTP/1.1 200 OK\n\n");
         client:send(buf);
-        client:close()
+        client:close();
         collectgarbage();
     end)
 end)
+
